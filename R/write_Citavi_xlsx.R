698b6db38fd731bf7d152bfb55a630d78e48f4de
Paul Schmidt <schmidtpaul@hotmail.de>
2021-02-22 15:11:09 +0100

    write_path & ...

--- R/write_Citavi_xlsx.R
+++ R/write_Citavi_xlsx.R
@@ -2,34 +2,44 @@
 #'
 #' @description
 #' `r lifecycle::badge("experimental")`
-#' #'
-#' @param CitDat A tibble of processed Citavi data TO DO
-#' @param read_path The path used for \code{\link[CitaviR]{read_Citavi_xlsx}}.
 #'
+#' @param CitDat A tibble.
+#' @param read_path The path/xlsx file name that was used for \code{\link[CitaviR]{read_Citavi_xlsx}}.
+#' @param write_path path/xlsx file name
+#' @param ... Other arguments passed to the [openxlsx::write.xlsx()] function.
 #'
 #' @examples
 #' \dontrun{
 #' your_path <- "data/yourCitaviExport.xlsx"
-#' read_Citavi_xlsx(your_path) %>%
-#'    find_obvious_duplicates %>%
-#'    handle_obvious_duplcates %>%
-#'    write_Citavi_xlsx(your_path)
+#'
+#' read_Citavi_xlsx(path = your_path) %>%
+#'    find_obvious_duplicates() %>%
+#'    write_Citavi_xlsx(read_path = your_path)
+#'
+#' read_Citavi_xlsx(path = your_path) %>%
+#'    find_obvious_duplicates() %>%
+#'    write_Citavi_xlsx(write_path = "output/yourNewFile.xlsx")
 #' }
 #'
-#' @return nothing ?! TO DO
 #' @importFrom openxlsx write.xlsx
 #' @export
 
-write_Citavi_xlsx <- function(CitDat, read_path = NULL) { # TO DO: Optionally choose export path
+write_Citavi_xlsx <- function(CitDat, read_path = NULL, write_path = NULL, ...) {
 
-  if (is.null(read_path)) {
-    stop("You did not provide a path to the imported Excel file")
-  } else {
-    last_dot    <- regexpr("\\.[^\\.]*$", read_path)[1]
-    export_path <- substr(read_path, 1, last_dot - 1)
-    export_path <- paste0(export_path, "_R_out.xlsx")
+  if (is.null(read_path) & is.null(write_path)) {
+    stop("You must provide a path!")
   }
 
-  openxlsx::write.xlsx(x = CitDat, file = export_path)
+  if (!is.null(read_path) & !is.null(write_path)) {
+    stop("You must not provide more than one path!")
+  }
+
+  if (is.character(read_path)) {
+    last_dot   <- regexpr("\\.[^\\.]*$", read_path)[1]
+    write_path <- substr(read_path, 1, last_dot - 1)
+    write_path <- paste0(write_path, "_R_out.xlsx")
+  }
+
+  openxlsx::write.xlsx(x = CitDat, file = write_path, ...)
 
 }
